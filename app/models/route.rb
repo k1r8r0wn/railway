1372c3f003c01572d0baa91bc8d91dfe70b0ad45
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-06-28 09:26:21 +0600

    Add relations & associations to new models
    1. Add train id to Ticket model;
    2. Add user id to Ticket model;

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,4 +2,6 @@ class Route < ActiveRecord::Base
   has_many :routes_stations
   has_many :stations, through: :routes_stations
   has_many :trains
+
+  validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627115656) do
+ActiveRecord::Schema.define(version: 20160628031321) do
 
   create_table "routes", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20160627115656) do
     t.string   "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "train_id"
+    t.integer  "user_id"
   end
 
   create_table "trains", force: :cascade do |t|
