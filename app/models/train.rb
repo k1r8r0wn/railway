be14ba5ca58e6f2b6b1cc513b437e1e1f212107a
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-06-28 15:23:20 +0600

    Add more info about carriages in train#show

--- app/models/train.rb
+++ app/models/train.rb
@@ -6,4 +6,26 @@ class Train < ActiveRecord::Base
   has_many   :carriages
 
   validates :number, presence: true
+
+  def train_details
+    self.carriages.group(:carriage_type).count
+  end
+
+
+  def sleeping_berths
+    self.carriages.where(carriage_type: 'sleeping').inject(Hash.new(0)) do |hash, carriage|
+      hash[:lower_berths] += carriage.lower_berths
+      hash[:upper_berths] += carriage.upper_berths
+      hash
+    end
+  end
+
+  def coupe_berths
+    self.carriages.where(carriage_type: 'coupe').inject(Hash.new(0)) do |hash, carriage|
+      hash[:lower_berths] += carriage.lower_berths
+      hash[:upper_berths] += carriage.upper_berths
+      hash
+    end
+  end
+
 end
